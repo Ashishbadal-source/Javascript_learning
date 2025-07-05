c81042500e01ac688227dcb08046d327972c52de
ASHISH BADAL <idh4816@gmail.com>
2025-07-05 14:15:27 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict";               // now it treats the full like according to newer js 
+//alert(3+3)               // we are using node.js not browser (n browser it shows an alert with the answer 6)
+
+let name = "Ashish"      //string
+let age = 20            //number
+let loggedIn = true    // boolean
+let state = null      // null datatype
+let value ;          // it is undefined
+
+/* 
+(PRIMITIVE DATA-TYPES)
+1.  number     =>   2 to power 53 
+2.  bigInt     =>   number that doesn't able to store in int(larger than int)
+3.  string     =>   " " or ' '
+4.  boolean    =>   true or false
+5.  null       =>   stand alone value  (and it falls under object datatype..LOL!😂) 
+6.  undefined  =>   value not assigned yet
+7.  symbol     =>   for uniqueness
+
+8.  object
+*/
+
+console.log(typeof null)           // answer is object
+console.log(typeof undefined)     // answer is undefined
+console.log(typeof "ashish")     // answer is string
