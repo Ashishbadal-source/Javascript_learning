2d539886440aeae288ba0f0555691431c0c130a2
ASHISH BADAL <idh4816@gmail.com>
2025-07-05 17:33:50 +0000

    Some tricky questions

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,3 +1,4 @@
+// Semicolons are optional in JavaScript
 let score1 = "18"
 let score2 = "18vk"
 let score3 = null
@@ -36,3 +37,49 @@ console.log(booleanlogged2)                                 // false for empty s
 
 let booleanlogged3 = Boolean(isloggedIn3)
 console.log(booleanlogged3)                               // true for non-empty string
+
+
+//**************************************  OPERATIONS  **********************************************//
+
+
+let val1 = 18
+let val2 = -val1
+console.log(val2)
+
+// console.log(121+23)             //  basic operations
+// console.log(121-23)
+// console.log(121*23)
+// console.log(121/23)
+// console.log(121**23)
+// console.log(121%23)
+
+
+let str1 = "Hello"
+let str2 = " Ashish"
+let str3 = str1 +str2
+console.log(str3)         //string concatenation
+
+
+console.log("1"+2)              // ans => 12
+console.log(1+"2")              // ans => 12
+console.log(1+2+"2")           // ans => 32 
+console.log("1"+2+2)         // ans => 122
+
+console.log(true)           // ans => true
+console.log(+true)          //ans => 1
+//console.log(true+)        //error
+console.log(true+17)        // ans => ( 1+17 = 18 )
+console.log(+"")            // ans => 0
+
+let num1,num2,num3
+num1 = num2 = num3 = 3 + 4
+
+let gamecounter = 100
+let x = gamecounter++        //post -increment
+let y = ++gamecounter       // pre-increment
+console.log(x)
+console.log(y)
+
+
+
+
